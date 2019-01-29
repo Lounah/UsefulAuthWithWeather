1dae73ef481a92165da7805213bf232fdcd35442
Lounah <lounah@yandex.ru>
2019-01-29 16:31:17 +0300

    Login fragment created & Login viewmodel created & Id of btn added in login.xml

--- app/src/main/java/com/lounah/usefulauthwithweather/presentation/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/lounah/usefulauthwithweather/presentation/authentication/AuthenticationActivity.kt
@@ -3,11 +3,21 @@ package com.lounah.usefulauthwithweather.presentation.authentication
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.lounah.usefulauthwithweather.R
+import com.lounah.usefulauthwithweather.presentation.authentication.login.LoginFragment
+import com.lounah.usefulauthwithweather.presentation.common.BaseActivity
 
-class AuthenticationActivity : AppCompatActivity() {
+class AuthenticationActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
+        setUpBaseFragment()
+    }
+
+    private fun setUpBaseFragment() {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container_main, LoginFragment())
+                .commit()
     }
 }
---
+++ app/src/main/java/com/lounah/usefulauthwithweather/presentation/authentication/login/LoginFragment.kt
@@ -0,0 +1,81 @@
+package com.lounah.usefulauthwithweather.presentation.authentication.login
+
+import android.arch.lifecycle.Observer
+import com.lounah.usefulauthwithweather.R
+import com.lounah.usefulauthwithweather.presentation.authentication.util.validator.CredentialsState
+import com.lounah.usefulauthwithweather.presentation.common.BaseFragment
+import kotlinx.android.synthetic.main.fragment_login.*
+import javax.inject.Inject
+
+class LoginFragment : BaseFragment() {
+    override val TAG = "login fragment"
+    override val layoutRes = R.layout.fragment_login
+
+    @Inject
+    lateinit var viewModel: LoginFragmentViewModel
+
+    override fun initUI() {
+        initViewModelObservers()
+        setUpLoginButton()
+        setUpRegistrationButton()
+    }
+
+    private fun initViewModelObservers() {
+        viewModel.errorState.observe(this, Observer { error ->
+            error?.let {
+                showToast(R.string.error_fetching_weather)
+            }
+        })
+
+        viewModel.loadingState.observe(this, Observer { isLoading ->
+            isLoading?.let {
+                // TODO: handle loading state...
+            }
+        })
+
+        viewModel.currentCredentialsState.observe(this, Observer { state ->
+            state?.let {
+                handleCredentialsState(it)
+            }
+        })
+
+        viewModel.weather.observe(this, Observer { weather ->
+            weather?.let {
+                showSnackBar(root_fragment_login, weather.temp)
+            }
+        })
+    }
+
+    private fun setUpLoginButton() {
+        clearErrorFields()
+        btn_log_in.setOnClickListener {
+            viewModel.updateCredentials(et_email.text.toString(), et_password.text.toString())
+        }
+    }
+
+    private fun setUpRegistrationButton() {
+        // ignore...
+    }
+
+    private fun handleCredentialsState(credentialsState: CredentialsState) {
+        when(credentialsState) {
+            CredentialsState.INVALID_CREDENTIALS -> {
+                et_email.error = resources.getString(R.string.invalid_username)
+                et_password.error = resources.getString(R.string.invalid_password)
+            }
+            CredentialsState.INVALID_PASSWORD -> {
+                clearErrorFields()
+                et_password.error = resources.getString(R.string.invalid_password)
+            }
+            CredentialsState.INVALID_USERNAME -> {
+                clearErrorFields()
+                et_email.error = resources.getString(R.string.invalid_username)
+            }
+        }
+    }
+
+    private fun clearErrorFields() {
+        et_email.error = null
+        et_password.error = null
+    }
+}
