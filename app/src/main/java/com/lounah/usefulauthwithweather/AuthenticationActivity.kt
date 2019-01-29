8aa9294c09ca2c994d2af275ea643b4b36ce21a9
Lounah <lounah@yandex.ru>
2019-01-29 15:00:43 +0300

    Initial commit

---
+++ app/src/main/java/com/lounah/usefulauthwithweather/AuthenticationActivity.kt
@@ -0,0 +1,12 @@
+package com.lounah.usefulauthwithweather
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthenticationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
